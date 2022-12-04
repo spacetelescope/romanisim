3adab02eef95da762e762cfc531890f4f0403de3
PaulHuwe <phuwe@stsci.edu>
2022-12-03 22:04:54 -0500

    Expanded ABFlux test.

--- romanisim/tests/test_bandpass.py
+++ romanisim/tests/test_bandpass.py
@@ -6,6 +6,7 @@ Unit tests for bandpass functions.  Tested routines:
 * get_abflux
 """
 
+import numpy as np
 from romanisim import bandpass
 from astropy.table import Table
 
@@ -39,6 +40,20 @@ def test_compute_abflux():
     assert ab_flux['F062'] == 49379170641.48872
     assert ab_flux['Grism_1stOrder'] == 75489060948.47092
     assert len(ab_flux.keys()) == 11
+
+    data_table = Table()
+    data_table['Wave'] = [0.60, 0.61, 0.62, 0.63, 0.64]
+    data_table['F062'] = [0.0, 0.0, 1.0, 0.0, 0.0]
+
+    ab062_flux = bandpass.compute_abflux(data_table)
+
+    data_table = Table()
+    data_table['Wave'] = [2.32, 2.33, 2.34, 2.35, 2.36]
+    data_table['F234'] = [0.0, 0.0, 1.0, 0.0, 0.0]
+
+    ab234_flux = bandpass.compute_abflux(data_table)
+
+    assert np.isclose( (ab234_flux['F234'] / ab062_flux['F062']), (1 / (234 / 62)), atol = 1.0e-6)
 
 
 def test_get_abflux():
