0d3e83c5579820d5fd316c0231666f9db0e2eecd
PaulHuwe <phuwe@stsci.edu>
2024-07-06 14:32:42 -0400

    Save

--- romanisim/l3.py
+++ romanisim/l3.py
@@ -26,7 +26,7 @@ import roman_datamodels.maker_utils as maker_utils
 CENTER_SCA = 2
 
 
-def add_objects_to_l3(l3_mos, source_cat, rng=None, seed=None):
+def add_objects_to_l3(l3_mos, xpos=None, ypos=None, coords=None, coords_unit='rad', wcs=None, psf=None, rng=None, seed=None):
     """Add objects to a Level 3 mosaic
 
     Parameters
@@ -42,28 +42,25 @@ def add_objects_to_l3(l3_mos, source_cat, rng=None, seed=None):
         l3_mos is updated in place
     """
 
-    if rng is None and seed is None:
-        seed = 143
-        log.warning(
-            'No RNG set, constructing a new default RNG from default seed.')
-    if rng is None:
-        rng = galsim.UniformDeviate(seed)
-
     # Obtain optical element
     filter_name = l3_mos.meta.basic.optical_element
 
-    # Generate WCS
-    twcs = romanisim.wcs.get_mosaic_wcs(l3_mos.meta, shape=l3_mos.data.shape)
+    if wcs is None:
+        # Generate WCS
+        wcs = romanisim.wcs.get_mosaic_wcs(l3_mos.meta, shape=l3_mos.data.shape)
 
-    # Create PSF
-    l3_psf = romanisim.psf.make_psf(filter_name=filter_name, sca=CENTER_SCA, chromatic=False, webbpsf=True)
+    if psf is None:
+        # Create PSF
+        psf = romanisim.psf.make_psf(filter_name=filter_name, sca=CENTER_SCA, chromatic=False, webbpsf=True)
 
-    # Generate x,y positions for sources
-    coords = np.array([[o.sky_pos.ra.rad, o.sky_pos.dec.rad]
-                       for o in source_cat])
-    sourcecountsall = galsim.ImageF(l3_mos.data.shape[0], l3_mos.data.shape[1], wcs=twcs, xmin=0, ymin=0)
-    # xpos, ypos = sourcecountsall.wcs._xy(coords[:, 0], coords[:, 1])
-    xpos, ypos = sourcecountsall.wcs.radecToxy(coords[:, 0], coords[:, 1], 'rad')
+    if coords is None:
+        # Generate x,y positions for sources
+        sourcecountsall = galsim.ImageF(l3_mos.data.shape[0], l3_mos.data.shape[1], wcs=wcs, xmin=0, ymin=0)
+        xpos, ypos = sourcecountsall.wcs.radecToxy(coords[:, 0], coords[:, 1], coords_unit)
+    elif all(pos is not None for pos in [xpos,ypos]):
+        log.error('No (xpos and ypos) or coords set. Adding zero objects.')
+        return
+
     xpos_idx = [round(x) for x in xpos]
     ypos_idx = [round(y) for y in ypos]
 
@@ -90,19 +87,19 @@ def add_objects_to_l3(l3_mos, source_cat, rng=None, seed=None):
             continue
 
         # Create empty postage stamp galsim source image
-        sourcecounts = galsim.ImageF(l3_mos.data.shape[0], l3_mos.data.shape[1], wcs=twcs, xmin=0, ymin=0)
+        sourcecounts = galsim.ImageF(l3_mos.data.shape[0], l3_mos.data.shape[1], wcs=wcs, xmin=0, ymin=0)
 
         # Simulate source postage stamp
         romanisim.image.add_objects_to_image(sourcecounts, [source_cat[idx]], xpos=[xpos[idx]],
-                                             ypos=[ypos[idx]], psf=l3_psf, flux_to_counts_factor=Ct,
+                                             ypos=[ypos[idx]], psf=psf, flux_to_counts_factor=Ct,
                                              bandpass=[filter_name], filter_name=filter_name, rng=rng)
 
         # Scale the source image back by its flux ratios
         sourcecounts /= Ct
 
         # Add sources to the original mosaic data array
-        # l3_mos.data = (l3_mos.data.value + sourcecounts.array) * l3_mos.data.unit
-        l3_mos.data = (l3_mos.data.value + np.swapaxes(sourcecounts.array, 0, 1)) * l3_mos.data.unit
+        l3_mos.data = (l3_mos.data.value + sourcecounts.array) * l3_mos.data.unit
+        # l3_mos.data = (l3_mos.data.value + np.swapaxes(sourcecounts.array, 0, 1)) * l3_mos.data.unit
 
         # Note for the future - other noise sources (read and flat) need to be implemented
 
@@ -130,7 +127,8 @@ def make_l3(image, context, metadata, exptimes, rng=None, seed=None,
 
     # code from apportion_counts_to_resultants
 
-    mosaic = np.swapaxes(image.array, 0, 1)
+    # mosaic = np.swapaxes(image.array, 0, 1)
+    mosaic = image.array.copy()
 
     # Set rng for creating readnoise, flat noise
     if rng is None and seed is None:
