ceccebffd55683b57d7cdacb6ecafe4e89addb5d
Eddie Schlafly <eschlafly@stsci.edu>
2024-09-11 10:47:41 -0400

    Increase log verbosity.

--- romanisim/__init__.py
+++ romanisim/__init__.py
@@ -17,5 +17,9 @@ import logging
 log = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+log.setLevel('INFO')
+# doesn't feel like it should be necessary, but it seems we don't get some
+# test log messages without setting setLevel('INFO') after configuring
+# with the basicConfig.
 
 __version__ = version(__name__)
