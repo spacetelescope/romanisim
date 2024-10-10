3c3927e06a3159dd1fcb99cb56354554a74aceaf
Eddie Schlafly <eschlafly@stsci.edu>
2024-10-10 16:46:55 -0400

    Remove ggsaa from file names.

--- scripts/romancal_make_regtest_l1s.sh
+++ scripts/romancal_make_regtest_l1s.sh
@@ -1,12 +1,12 @@
-# r0000101001001001001_01101_0001_WFI01 - default for most steps
-# r0000201001001001001_01101_0001_WFI01 - equivalent for spectroscopic data
-# r0000101001001001001_01101_0002_WFI01 - a second resample exposure, only cal step needed
-# r0000101001001001001_01101_0003_WFI01 - for ramp fitting; truncated image
-# r0000201001001001001_01101_0003_WFI01 - for ramp fitting; truncated spectroscopic
+# r0000101001001001001_0001_WFI01 - default for most steps
+# r0000201001001001001_0001_WFI01 - equivalent for spectroscopic data
+# r0000101001001001001_0002_WFI01 - a second resample exposure, only cal step needed
+# r0000101001001001001_0003_WFI01 - for ramp fitting; truncated image
+# r0000201001001001001_0003_WFI01 - for ramp fitting; truncated spectroscopic
 #                                         we need only darkcurrent & ramp fit for these
 #
-# r0000101001001001001_01101_0004_WFI01 - ma_table 110, 16 resultants file
-# r0000201001001001001_01101_0004_WFI01 - ma_table 110, 16 resultant file, spectroscopic
+# r0000101001001001001_0004_WFI01 - ma_table 110, 16 resultants file
+# r0000201001001001001_0004_WFI01 - ma_table 110, 16 resultant file, spectroscopic
 
 # note that the "spectroscopic" files are really imaging files where the only
 # thing that has been updated is the optical_element and exposure.type.
@@ -14,18 +14,18 @@
 # but I haven't done anything here.
 
 # default image
-romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:00:00 --rng_seed 1 --drop-extra-dq r0000101001001001001_01101_0001_WFI01_uncal.asdf &
+romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:00:00 --rng_seed 1 --drop-extra-dq r0000101001001001001_0001_WFI01_uncal.asdf &
 # different location, different activity
-romanisim-make-image --radec 270.00 66.01 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:05:00 --rng_seed 2 --drop-extra-dq r0000101001001001001_01101_0002_WFI01_uncal.asdf &
+romanisim-make-image --radec 270.00 66.01 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:05:00 --rng_seed 2 --drop-extra-dq r0000101001001001001_0002_WFI01_uncal.asdf &
 # default spectroscopic image
-romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:10:00 --rng_seed 3 --drop-extra-dq r0000201001001001001_01101_0001_WFI01_uncal.asdf --pretend-spectral GRISM &
+romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:10:00 --rng_seed 3 --drop-extra-dq r0000201001001001001_0001_WFI01_uncal.asdf --pretend-spectral GRISM &
 # truncated image
-romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:15:00 --rng_seed 4 --drop-extra-dq r0000101001001001001_01101_0003_WFI01_uncal.asdf --truncate 6 &
+romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:15:00 --rng_seed 4 --drop-extra-dq r0000101001001001001_0003_WFI01_uncal.asdf --truncate 6 &
 # truncated spectroscopic image
-romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:20:00 --rng_seed 5 --drop-extra-dq r0000201001001001001_01101_0003_WFI01_uncal.asdf --truncate 6 --pretend-spectral GRISM &
+romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:20:00 --rng_seed 5 --drop-extra-dq r0000201001001001001_0003_WFI01_uncal.asdf --truncate 6 --pretend-spectral GRISM &
 # 16 resultant image
-romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 110 --date 2027-06-01T00:25:00 --rng_seed 6 --drop-extra-dq r0000101001001001001_01101_0004_WFI01_uncal.asdf &
+romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 110 --date 2027-06-01T00:25:00 --rng_seed 6 --drop-extra-dq r0000101001001001001_0004_WFI01_uncal.asdf &
 # truncated spectroscopic image
-romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 110 --date 2027-06-01T00:30:00 --rng_seed 7 --drop-extra-dq r0000201001001001001_01101_0004_WFI01_uncal.asdf --pretend-spectral GRISM &
+romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --webbpsf --usecrds --ma_table_number 110 --date 2027-06-01T00:30:00 --rng_seed 7 --drop-extra-dq r0000201001001001001_0004_WFI01_uncal.asdf --pretend-spectral GRISM &
 
 wait
