5daff9264a1f6a708a98897003678aa69f2c5f4e
Nikhil Padmanabhan <nikhil.padmanabhan@gmail.com>
2025-09-12 03:20:00 -0400

    Handle MA Table numbers not in parameters

--- romanisim/image.py
+++ romanisim/image.py
@@ -773,9 +773,15 @@ def simulate(metadata, objlist,
 
     filter_name = image_mod.meta.instrument.optical_element
 
-    read_pattern = metadata['exposure'].get(
-        'read_pattern',
-        parameters.read_pattern[metadata['exposure']['ma_table_number']])
+    # This logic fails, if the default parameters dictionary does not 
+    # have the appropriate MA table. 
+    #read_pattern = metadata['exposure'].get(
+    #    'read_pattern',
+    #    parameters.read_pattern[metadata['exposure']['ma_table_number']])
+    if 'read_pattern' in metadata['exposure']:
+        read_pattern = metadata['exposure']['read_pattern']
+    else:
+        read_pattern = parameters.read_pattern[metadata['exposure']['ma_table_number']]
 
     refdata = gather_reference_data(image_mod, usecrds=usecrds)
     read_noise = refdata['readnoise']
