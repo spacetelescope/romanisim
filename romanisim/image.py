c2d278550b34af52047476375802538015c5da96
Nikhil Padmanabhan <nikhil.padmanabhan@gmail.com>
2025-09-18 17:11:41 -0400

    Fix up the exposure time by getting the right read time from the MA table if available

--- romanisim/image.py
+++ romanisim/image.py
@@ -669,7 +669,9 @@ def simulate_counts(metadata, objlist,
         read_pattern = parameters.read_pattern[metadata['exposure']['ma_table_number']]
 
     sca = int(metadata['instrument']['detector'][3:])
-    exptime = parameters.read_time * read_pattern[-1][-1]
+    read_time = metadata['exposure'].get('frame_time', parameters.read_time)
+    exptime = read_time * read_pattern[-1][-1]
+    log.info(f"Using an exposure time of {exptime}")
     if rng is None and seed is None:
         seed = 43
         log.warning(
@@ -1255,6 +1257,7 @@ def inject_sources_into_l2(model, cat, x=None, y=None, psf=None, seed=50,
     bandpass = models.bandpass.getBandpasses(AB_zeropoint=True)[galsim_filter_name]
     abflux = models.bandpass.get_abflux(filter_name, sca)
     read_pattern = model.meta.exposure.read_pattern
+    # Should we update read_time to model.meta.exposure.frame_time?
     exptime = parameters.read_time * read_pattern[-1][-1]
     tij = romanisim.l1.read_pattern_to_tij(read_pattern)
     tbar = ramp.read_pattern_to_tbar(read_pattern)
