2416e92757bf5adbf10b921f18d87e5b1fe33831
Eddie Schlafly <eschlafly@stsci.edu>
2026-01-14 12:37:41 -0500

    also do var_poisson as float16.

--- romanisim/image.py
+++ romanisim/image.py
@@ -1068,11 +1068,23 @@ def make_asdf(slope, slopevar_rn, slopevar_poisson, metadata=None,
     out['dq'] = np.zeros(slope.shape, dtype='u4')
     if dq is not None:
         out['dq'][:, :] = dq
-    out['var_poisson'] = slopevar_poisson
-    out['var_rnoise'] = slopevar_rn
-    out['var_flat'] = slopevar_rn * 0
-    err = np.sqrt(out['var_poisson'] + out['var_rnoise'] + out['var_flat'])
-    out['err'] = err.astype(out['err'].dtype)
+
+    def assign_with_default_types(fielddict, out):
+        for field in fielddict:
+            if out.get(field, None) is not None:
+                dtype = out[field].dtype
+            else:
+                dtype = fielddict[field][1]
+            out[field] = fielddict[field][0].astype(dtype)
+
+    fielddict = dict(
+        var_poisson=(slopevar_poisson, np.float16),
+        var_rnoise=(slopevar_rn, np.float16),
+        var_flat=(slopevar_rn * 0, np.float16),
+        err=(np.sqrt(slopevar_poisson + slopevar_rn), np.float16),
+    )
+    assign_with_default_types(fielddict, out)
+
     out['amp33'] = np.zeros((n_groups, 4096, 128), dtype=out.amp33.dtype)
     for side in ('left', 'right', 'top', 'bottom'):
         if side in ('left', 'right'):
