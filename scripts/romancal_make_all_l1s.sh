903a1b4a725009b8158cda3d1dc9fd1c454fb670
Brett <brettgraham@gmail.com>
2026-03-05 11:49:29 -0500

    only 1 sca

--- scripts/romancal_make_all_l1s.sh
+++ scripts/romancal_make_all_l1s.sh
@@ -19,7 +19,7 @@
 set -e
 
 # default image
-romanisim-make-image --radec 270.00 66.00 --level 1 --sca -1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --psftype stpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:00:00 --rng_seed 1 --drop-extra-dq r0000101001001001001_0001_{}_{bandpass}_uncal.asdf
+romanisim-make-image --radec 270.00 66.00 --level 1 --sca 1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --psftype stpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:00:00 --rng_seed 1 --drop-extra-dq r0000101001001001001_0001_{}_{bandpass}_uncal.asdf
 # # second image at a different location, different activity
 # romanisim-make-image --radec 270.00 66.01 --level 1 --sca -1 --bandpass F158 --catalog gaia-270-66-2027-06-01.ecsv --psftype stpsf --usecrds --ma_table_number 109 --date 2027-06-01T00:05:00 --rng_seed 2 --drop-extra-dq r0000101001001001001_0002_{}_{bandpass}_uncal.asdf
 # # default spectroscopic image
